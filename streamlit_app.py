d193ec9d36968e29fa3a6610f1df35c81bf07d7a
hskjerping <89133679+hskjerping@users.noreply.github.com>
2021-08-20 09:52:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
     
-    def_h=200,
+    def_h=200
     cur_h=st.number_input('Height', min_value=0.0, max_value=None, value=def_h, step=1)
     
     Point = namedtuple('Point', 'x y')
