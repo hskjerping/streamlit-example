efad061366eef7bc771b3492af5870e1a14e6072
hskjerping <89133679+hskjerping@users.noreply.github.com>
2021-08-25 14:02:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,67 +49,14 @@ with user_input:
     moving_average_day = st.sidebar.slider('How many days to consider for the moving average? ', min_value = 5, max_value = 14, value = 7, step=1)
 
     # Creating the dataframe for the county
-    df_county = df[(df.county == county)& (df.state == state)].copy()
+    df_county = df[(df.cam == cam)].copy()
 
     #Create a new column with new cases
-    df_county['new_cases'] = df_county.loc[:, 'cases'].diff()
+    df_county['ifov'] = df_county.loc[:, 'ifov'].diff()
 
     #Create a new column for 7-day moving average
-    df_county['moving_average'] = df_county.loc[:,'new_cases'].rolling(window=moving_average_day).mean()
+    df_county['framerate'] = df_county.loc[:,'framerate']
 
     #Create a 
 
-with output_graphs:
-    
-    # Summary Table
 
-    st.header(f'Summary Table for the last {table_days} days.')
-    
-    st.markdown(""" This table includes the number of cases, deaths, new cases and moving average for your selection.""")
-
-    #st.write(df_county.iloc[-table_days:,-4:])
-
-    a = df_county.iloc[-table_days:, -4:]
-    
-    my_table = st.table(a)
-
-
-    # Total Cases Graph
-
-    st.header(f'Total Cases for {county},{state}.')
-    
-    total_cases_chart = df_county['cases']
-
-    
-    st.line_chart(total_cases_chart)
-
-    st.markdown("""**Note:** You can zoom on this graph if you are in front of a Desktop or Laptop by using your scrolling wheel on your mouse. You can also point on the line to get more information.""")
-
-    # Moving Average Graph
-
-    st.header(f'{moving_average_day} moving average for {county},{state}.')
-    
-    moving_average_chart = df_county['moving_average']
-    
-    st.line_chart(moving_average_chart)
-
-    st.markdown("""**Note:** You can zoom on this graph if you are in front of a Desktop or Laptop by using your scrolling wheel on your mouse. You can also point on the line to get more information.""")
-
-    # Death Graph
-
-    st.header(f'Total Deaths for {county},{state}.')
-    
-    total_deaths_chart = df_county['deaths']
-    
-    st.line_chart(total_deaths_chart)
-
-    st.markdown("""**Note:** You can zoom on this graph if you are in front of a Desktop or Laptop by using your scrolling wheel on your mouse. You can also point on the line to get more information.""")
-
-with author_credits:
-    st.header(f'Credits')
-    st.markdown("""
-    **Thank you for using my application!**
-    
-    The dataset used to feed this application is provided by [New York Times Covid-19 Github Repository](https://github.com/nytimes/covid-19-data).
-    This application uses the Streamlit package library. You can learn more about me and my other projects by visiting my website [Not A Programmer] (https://notaprogrammer.com) or [my Github Repo] (https://github.com/cerratom).    
-    """)
